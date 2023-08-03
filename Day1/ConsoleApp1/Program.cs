045ade0cda94dafca1d9f009343fa96263e0f833
Abdurehman Mustafa <abdurehamanmustefa@gmail.com>
2023-08-03 16:25:59 +0300

    days

---
+++ Day1/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace HelloWorld{
+    class MyGradeCalculator{
+
+        private string name;
+        private readonly int numCourses;
+        private readonly double[] grades;
+        static void Main(string[] args){
+            Console.WriteLine("Welcome to Grade Calculator!");
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine(name);
+
+            Console.WriteLine("Please enter the number of courses: ");
+            int numCourses = Convert.ToInt32(Console.ReadLine());
+
+            MyGradeCalculator grade = new (name, numCourses);
+
+            double gpa = grade.CalculateGrade();
+
+            Console.WriteLine("Your GPA is: " + gpa);
+        }
+
+        public MyGradeCalculator(string name, int numCourses){
+            this.name = name;
+            this.numCourses = numCourses;
+            this.grades = new double[numCourses];
+        }
+
+        public double CalculateGrade(){
+            for(int i = 0; i < numCourses; i++){
+                Console.WriteLine("Please enter the grade for course " + (i+1) + ": ");
+                var grade = Convert.ToInt32(Console.ReadLine());
+                if( grade > 100 || grade < 0){
+                    Console.WriteLine("Invalid grade. Please enter a valid grade: ");
+                    i--;
+                    continue;
+                }
+                else if(grade >= 85){
+                    grades[i] = 4;
+                }
+                else if(grade >= 80){
+                    grades[i] = 3.75;
+                }
+                else if(grade >= 75){
+                    grades[i] = 3.5;
+                }
+                else if(grade >= 70){
+                    grades[i] = 3;
+                }
+                else if(grade >= 65){
+                    grades[i] = 2.75;
+                }
+                else if(grade >= 60){
+                    grades[i] = 2.5;
+                }
+                else if(grade >= 55){
+                    grades[i] = 2;
+                }
+                else if(grade >= 50){
+                    grades[i] = 1.75;
+                }
+                else if(grade >= 45){
+                    grades[i] = 1.5;
+                }
+                else if(grade >= 40){
+                    grades[i] = 1;
+                }
+                else{
+                    grades[i] = 0;
+                }
+            }
+
+            double total = 0;
+            for(int i = 0; i < numCourses; i++){
+                total += grades[i];
+            }
+
+            double gpa = total / numCourses;
+
+            return gpa;
+        }
+        
+    }
+}
---
+++ Day1/Dictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿// create a dictionary with key as string and value as string
+// add 3 elements to the dictionary
+// print the dictionary
+
+using System;
+using System.Collections.Generic;
+
+namespace LearnCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String: ");
+            var str = Console.ReadLine();
+
+            Program p = new Program();
+            var result = p.Frequency(str);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("Key: " + item.Key + ", Value: " + item.Value);
+            }
+        }
+
+        public Dictionary<string, int> Frequency(string str)
+        {
+            Dictionary<string, int> dict = new();
+            string[] words = str.Split(' ');
+            foreach (var word in words)
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word] = dict[word] + 1;
+                }
+                else
+                {
+                    dict.Add(word, 1);
+                }
+            }
+            return dict;
+        }
+            
+    }
+}
+
+// Key: 1, Value: One
